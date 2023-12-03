f2eb5f1457aed723fb182c7f3ddfb6bd207cacd4
tamakiii <tamakiii@users.noreply.github.com>
2023-12-03 19:36:07 +0900

    add argument

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket
+import argparse
 
-def udp_server(host='0.0.0.0', port=16384):
+def udp_server(host, port):
     # Create a socket for UDP
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -14,5 +15,14 @@ def udp_server(host='0.0.0.0', port=16384):
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         print(f"Received message: {data.decode()} from {addr}")
 
+def main():
+    parser = argparse.ArgumentParser(description='UDP Server')
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='Host to listen on')
+    parser.add_argument('--port', type=int, default=16384, help='Port to listen on')
+
+    args = parser.parse_args()
+
+    udp_server(args.host, args.port)
+
 if __name__ == "__main__":
-    udp_server()
+    main()
