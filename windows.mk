257853c59a418905be468c98aac257a146d57b11
tamakiii <tamakiii@users.noreply.github.com>
2023-12-03 21:57:37 +0900

    add ip addresse

--- windows.mk
+++ windows.mk
@@ -4,6 +4,8 @@ AUDIO_DEVICE :=
 CONTAINER_ID := $(shell docker ps -qf ancestor=tamakiii-sandbox/hello-docker-rtp)
 CONTAINER_ADDRESS := $(shell docker inspect "$(CONTAINER_ID)" | jq -r ".[0].NetworkSettings.Networks.bridge.IPAddress")
 CONTAINER_PORT := 16384
+INTERFACE := Ethernet
+IP_ADDRESS = $(shell python address.py | jq -r '.[] | select(.interface == "$(INTERFACE)").ip_address')
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -14,9 +16,10 @@ list:
 info:
 	@echo CONTAINER_ADDRESS=$(CONTAINER_ADDRESS)
 	@echo CONTAINER_PORT=$(CONTAINER_PORT)
+	@echo IP_ADDRESS=$(IP_ADDRESS)
 
 up:
 	ffmpeg -f dshow -i audio="$(AUDIO_DEVICE)" -acodec libopus -f rtp rtp://$(CONTAINER_ADDRESS):$(CONTAINER_PORT)
 
 send:
-	python send.py
+	python send.py --host $(IP_ADDRESS)
