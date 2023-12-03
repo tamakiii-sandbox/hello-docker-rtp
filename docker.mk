77efe952aa64218b9b546225b68b1b4b858445ec
tamakiii <tamakiii@users.noreply.github.com>
2023-12-03 17:55:13 +0900

    add info rule and port binding (udp)

--- docker.mk
+++ docker.mk
@@ -1,15 +1,18 @@
-.PHONY: help build bash clean
+.PHONY: help info build bash clean
 
 IMAGE_NAME = "tamakiii-sandbox/hello-docker-rtp"
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
+info:
+	docker inspect $(shell docker ps -qf "ancestor=$(IMAGE_NAME)") | jq -r ".[0].NetworkSettings.Networks.bridge.IPAddress"
+
 build: Dockerfile
 	docker build -t $(IMAGE_NAME) .
 
 bash:
-	docker run --rm -it -e TZ=Asia/Tokyo $(IMAGE_NAME) bash
+	docker run --rm -it -e TZ=Asia/Tokyo -v $(shell pwd):/app -w /app -p 16384:16384/udp $(IMAGE_NAME) bash
 
 clean:
 	docker rmi $(IMAGE_NAME)
